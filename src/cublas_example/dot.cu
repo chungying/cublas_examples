50bf7cde1f06e05947a1abeeb7c36125e034f8b7
chungying <chungying.li@ntnu.edu.tw>
2018-04-02 18:47:37 +0100

    create Ddot example

---
+++ src/cublas_example/dot.cu
@@ -0,0 +1,36 @@
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <stdio.h>
+
+int main(void)
+{
+    const int nvals = 10;
+    const size_t sz = sizeof(double) * (size_t)nvals;
+    double x[nvals], y[nvals];
+    double *x_, *y_, *result_;
+    double result=0., resulth=0.;
+
+    for(int i=0; i<nvals; i++) {
+        x[i] = y[i] = (double)(i)/(double)(nvals);
+        resulth += x[i] * y[i];
+    }
+
+    cublasHandle_t h;
+    cublasCreate(&h);
+    cublasSetPointerMode(h, CUBLAS_POINTER_MODE_DEVICE);
+
+    cudaMalloc( (void **)(&x_), sz);
+    cudaMalloc( (void **)(&y_), sz);
+    cudaMalloc( (void **)(&result_), sizeof(double) );
+
+    cudaMemcpy(x_, x, sz, cudaMemcpyHostToDevice);
+    cudaMemcpy(y_, y, sz, cudaMemcpyHostToDevice);
+
+    cublasDdot(h, nvals, x_, 1, y_, 1, result_);
+
+    cudaMemcpy(&result, result_, sizeof(double), cudaMemcpyDeviceToHost);
+
+    printf("%f %f\n", resulth, result);
+
+    return 0;
+}
